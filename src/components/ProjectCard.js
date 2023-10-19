e31627194053ac42e91ccb6d63da0f445062e9b2
Dicky Sanjaya <dickysanjaya003@gmail.com>
2023-10-19 13:03:00 +0700

    add link href & img alt

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,7 +5,7 @@ export const ProjectCard = ({ title, description, imgUrl, liveUrl }) => {
     <Col size={12} sm={6} md={4}>
       <a href={liveUrl}>
         <div className="proj-imgbx" type="link">
-          <img src={imgUrl} target='_blank'/>
+          <img src={imgUrl} target='_blank' alt={title}/>
           <div className="proj-txtx">
             <h4 className="text-white">{title}</h4>
             <span className="text-white">{description}</span>
